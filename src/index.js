be8be2ccc97750e830f3575e4c5899411f4080c0
Denis Korotaevsky <huligan8771@gmail.com>
2023-08-17 01:32:05 +0300

    axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello!')
+import 'regenerator-runtime/runtime'
+import { API_URL, URL_COMICS } from './constants/api';
+import { getDataApi } from './utils/getDataApi';
+
+(async () => {
+    const data = await getDataApi.getData(API_URL + URL_COMICS);
+    console.log(data)
+})();
